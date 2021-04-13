295378a83ce985cba75d23bcbba5206ad2e596d4
DefiantHuman <82457606+DefiantHuman@users.noreply.github.com>
2021-04-13 11:39:36 -0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,2 +1,8 @@
-import tkinter as tk
-
+from tkinter import *
+display_text = "Testing"
+window = Tk()
+window.title("HTML")
+window.configure(width=800, height=800)
+b1 = Button(window, text = "START")
+b1.pack()
+window.mainloop()
