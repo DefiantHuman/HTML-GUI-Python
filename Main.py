d852069e3c5551a88958c22aec2317d2e35429c4
DefiantHuman <82457606+DefiantHuman@users.noreply.github.com>
2021-04-13 15:06:06 -0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+def Close():
+    window.destroy()
 display_text = "Welcome! This is a Python based html editor."
 window = Tk()
 window.title("HTML")
@@ -6,7 +8,9 @@ window.configure(width=800, height=800)
 T = Text(window, height = 5, width = 52)
 T.insert(END, display_text)
 b1 = Button(window, text = "START")
+b2 = Button(window, text = "EXIT", command=Close)
 T["state"] = DISABLED
 T.pack()
 b1.pack()
+b2.pack()
 window.mainloop()
