9754c715f5c532d9d7c22c7682decfbe8db11ad5
DefiantHuman <82457606+DefiantHuman@users.noreply.github.com>
2021-04-13 12:01:54 -0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,11 +1,12 @@
 from tkinter import *
-display_text = "Testing"
+display_text = "Welcome! This is a Python based html editor."
 window = Tk()
 window.title("HTML")
 window.configure(width=800, height=800)
 T = Text(window, height = 5, width = 52)
 T.insert(END, display_text)
 b1 = Button(window, text = "START")
+T["state"] = DISABLED
 T.pack()
 b1.pack()
 window.mainloop()
